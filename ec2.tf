0ddc40468bb6097ef608dda84a27b6377e9a248f
Singuluri pavan kumar <150420006+PavanGitPro@users.noreply.github.com>
2024-03-28 00:15:39 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
 # Creating EC2 Instance
 resource "aws_instance" "wordpress4" {
-  ami                         = "ami-02d7fd1c2af6eead0"
+  ami                         = "ami-033a1ebf088e56e81"
   instance_type               = "t2.micro"
-  key_name                    = "master1"
+  key_name                    = "master-slave"
   vpc_security_group_ids      = [aws_security_group.ec2-SG.id]
   subnet_id                   = aws_subnet.subnet-1.id
   associate_public_ip_address = true
