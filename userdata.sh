5042f300c314804193de836689f494c76af4d3e9
Singuluri pavan kumar <150420006+PavanGitPro@users.noreply.github.com>
2024-03-28 00:17:24 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -6,6 +6,6 @@ sudo systemctl enable docker
 sudo chmod 666 /var/run/docker.sock
 sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-git clone https://github.com/nissi1234/wordpress.git
+git clone https://github.com/PavanGitPro/wordpress.git
 cd wordpress
 docker-compose up -d
